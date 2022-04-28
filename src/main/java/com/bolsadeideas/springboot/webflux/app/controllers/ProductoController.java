6dcf9e9d4d36ca6cd1b180a70e0c9ea9b5870b40
Martín <magadiflo@gmail.com>
2022-04-28 11:27:08 -0500

    Modo Chunked para manejar la contrapresión

--- src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -53,5 +53,19 @@ public class ProductoController {
 		model.addAttribute("titulo", "Listado de productos");
 		return "listar";
 	}
+	
+	@GetMapping("/listar-full")
+	public String listarFull(Model model) {
+		Flux<Producto> productos = this.productoDao.findAll()
+				.map(producto -> {
+					producto.setNombre(producto.getNombre().toUpperCase());
+					return producto;
+				})
+				.repeat(5000);
+		
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar";
+	}
 
 }
