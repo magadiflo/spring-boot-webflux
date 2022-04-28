4b2b94f0a8fcc563a7c41a3bbd0dac2d53229b6e
Martín <magadiflo@gmail.com>
2022-04-28 11:19:14 -0500

    Modo Reactive Data Driven para manejar la contrapresión

--- src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -1,11 +1,14 @@
 package com.bolsadeideas.springboot.webflux.app.controllers;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.bolsadeideas.springboot.webflux.app.models.dao.IProductoDao;
 import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
@@ -31,6 +34,22 @@ public class ProductoController {
 		productos.subscribe(prod -> log.info(prod.getNombre()));
 		
 		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar";
+	}
+	
+	@GetMapping("/listar-data-driver")
+	public String listarDataDriver(Model model) {
+		Flux<Producto> productos = this.productoDao.findAll()
+				.map(producto -> {
+					producto.setNombre(producto.getNombre().toUpperCase());
+					return producto;
+				})
+				.delayElements(Duration.ofSeconds(1));
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		
+		model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 2));
 		model.addAttribute("titulo", "Listado de productos");
 		return "listar";
 	}
